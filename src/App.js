b0f72d225cb2e44ff0e84181200ddc1e8f9e2f99
Patryk Poźniak <patryk.pozniak@avid.com>
2018-08-22 11:02:22 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="icon-logo">
+        <span className="sr-only">Yoti</span>
+        </div>
+        <span className="icon-verified"></span>
+        <h1>Yoti app</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
