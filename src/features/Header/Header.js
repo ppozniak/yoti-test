d8746aa0e0fe8927acc6edb6d6919b05e821f5b2
Patryk Poźniak <ppozniak95@gmail.com>
2018-08-23 08:40:35 +0200

    Header button and avatar

--- src/features/Header/Header.js
+++ src/features/Header/Header.js
@@ -8,6 +8,19 @@ const Header = () => (
         <span className="sr-only">Yoti</span>
       </a>
     </div>
+    <div className={styles.rightWrapper}>
+      <a href="#connect" className={styles.connectedButton}>
+        <span>Connected</span>
+        <div className="icon-connect-on" />
+      </a>
+      <a className={styles.profileLink} href="/" title="Your profile">
+        <img
+          src="http://place-sloth.com/75-75.png"
+          alt=""
+          className={`img-circle ${styles.profileImg}`}
+        />
+      </a>
+    </div>
   </header>
 );
 
