ac8a9ae3857a03a66951ac3c0686c18647e56719
Patryk Poźniak <ppozniak95@gmail.com>
2018-08-23 09:10:25 +0200

    Replace sprites, sidebar button

--- src/features/Sidebar/Sidebar.js
+++ src/features/Sidebar/Sidebar.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import styles from './sidebar.scss';
 
-const Sidebar = () => <div className={styles.sidebar}> This is a sidebar.</div>;
+const MenuItem = () => (
+  <li className={styles.sidebar__item}>
+    <a href="#activity" className={styles.sidebar__link}>
+      <div className="icon-activity" />
+      <span className="p-xs-l-16">Activity</span>
+    </a>
+  </li>
+);
+
+const Sidebar = () => (
+  <nav className={styles.sidebar}>
+    <ul className={styles.sidebar__list}>
+      <MenuItem />
+    </ul>
+  </nav>
+);
 
 export default Sidebar;
